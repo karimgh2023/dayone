5e4e00b335510dc07c82c252d804e8c169f232e3
karim <karimghammam9@gmail.com>
2025-05-09 17:23:25 +0200

    add employee/chef_department  implimentation

--- src/app/shared/directives/sortable.directive.ts
+++ src/app/shared/directives/sortable.directive.ts
@@ -1,13 +1,11 @@
 import {Directive, EventEmitter, Input, Output} from '@angular/core';
 import { attendanceByUserType } from '../../componets/dashbord/hrmdashboards/attendance/attendencebyuser/attendenceByUserTableData';
-import { employeeList } from '../../componets/dashbord/hrmdashboards/employess/employee-list/employeeListTableData';
 import { expensesList } from '../../componets/dashbord/hrmdashboards/expenses/expensesTableData';
 import { noticeBoardList } from '../../componets/dashbord/hrmdashboards/notice-board/noticaBoardTableData';
 import { employeeAttendanceList } from '../../componets/dashbord/employee-dashboard/attendance/employeeAttendanceTableData';
 
 export type SortColumn = keyof noticeBoardList | '';
 export type ExpensesSortColumn = keyof expensesList | '';
-export type employeeSortColumn = keyof employeeList | '';
 export type AttendanceByUserSortColumn = keyof attendanceByUserType | '';
 export type employeeAttendanceSortColumn = keyof employeeAttendanceList | '';
 // export type SortCountryColumn = keyof Country | '';
